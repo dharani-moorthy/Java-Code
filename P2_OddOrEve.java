f43649e752f5a05066637b513b876a8d8c08f987
Dharani M <dharanimoorthy51@gmail.com>
2025-12-30 20:24:53 +0530

    Odd Or Even

---
+++ P2_OddOrEve.java
@@ -0,0 +1,11 @@
+package PrepInsta;
+//Check Whether a Number is Even or Odd in Java
+public class P2_OddOrEve {
+    public static void main(String[] args) {
+        int num=1;
+        String result = num%2==0?"Even":"Odd";
+        System.out.println(result);
+        // if(even) else (odd)
+        // condition if:else;
+    }
+}
