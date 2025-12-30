1efeabc1f124f99ab070ea9891bb0382c7cdc446
Dharani M <dharanimoorthy51@gmail.com>
2025-12-30 20:27:46 +0530

    Greatest Of Two Number

---
+++ P6_GreatestOfTwoNum.java
@@ -0,0 +1,14 @@
+package PrepInsta;
+//Find the Greatest of the Two Numbers in Java
+public class P6_GreatestOfTwoNum {
+    public static void main(String[] args) {
+        int num1=12;
+        int num2=9;
+        int result= num1>num2? num1:num2;
+        System.out.println(result);
+        /*
+        int a=Math.max(num1,num1);
+        System.out.println(a);
+        */
+    }
+}
