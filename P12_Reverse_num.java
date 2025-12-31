e7323a61e43fbe20fba631016442d4da1dc3b844
Dharani M <dharanimoorthy51@gmail.com>
2025-12-31 13:47:21 +0530

    Reverse Number

---
+++ P12_Reverse_num.java
@@ -0,0 +1,14 @@
+package PrepInsta;
+//Find the Reverse of a Number in Java Language
+public class P12_Reverse_num {
+    public static void main(String[] args) {
+        int n=123566; int rev=0,rem;
+        while (n!=0){
+            rem=n%10;
+            rev=rev*10+rem;
+            n/=10;
+        }
+        System.out.println(rev);
+
+        }
+    }
