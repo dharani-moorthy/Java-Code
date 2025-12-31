1a744e30b0bea92835986be44133a6b7a7a08787
Dharani M <dharanimoorthy51@gmail.com>
2025-12-31 13:51:50 +0530

    Power of Number

---
+++ P19_Power_Of_Num.java
@@ -0,0 +1,10 @@
+package PrepInsta;
+//Find the Power of a Number in Java Language
+public class P19_Power_Of_Num {
+    public static void main(String[] args) {
+        double a=2.0;
+        double b=3.0;
+        double c=Math.pow(a,b);
+        System.out.println(c);
+    }
+}
