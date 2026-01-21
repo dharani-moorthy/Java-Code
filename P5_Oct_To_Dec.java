2a7e211837bd6667eda53c344b5a3df5df83a145
Dharani M <dharanimoorthy51@gmail.com>
2026-01-21 21:13:56 +0530

    Oct to Dec

---
+++ P5_Oct_To_Dec.java
@@ -0,0 +1,9 @@
+package PrepInsta_BasedOnNumbers;
+//Octal to decimal conversion using Java
+public class P5_Oct_To_Dec {
+    public static void main(String[] args) {
+        String a="512";
+        int b=Integer.parseInt(a,8);
+        System.out.println(b);
+    }
+}
