996b3aa883fdd72d392cf1535a4d819f85494844
Dharani M <dharanimoorthy51@gmail.com>
2026-01-21 21:13:33 +0530

    Binary to Dec

---
+++ P4_Bin_To_Dec.java
@@ -0,0 +1,9 @@
+package PrepInsta_BasedOnNumbers;
+//Binary to decimal conversion using Java
+public class P4_Bin_To_Dec {
+    public static void main(String[] args) {
+        String num="111001";
+        int decimal=Integer.parseInt(num,2);
+        System.out.println(decimal);
+    }
+}
