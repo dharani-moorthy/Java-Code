84ba740bb0c2841745e9092de5773fa942ad4712
Dharani M <dharanimoorthy51@gmail.com>
2026-01-25 14:06:01 +0530

    Dec to Oct

---
+++ P8_Dec_To_Oct.java
@@ -0,0 +1,9 @@
+package PrepInsta_BasedOnNumbers;
+//Decimal to octal conversion using Java
+public class P8_Dec_To_Oct {
+    public static void main(String[] args) {
+        int a=148;
+        String b=Integer.toOctalString(a);
+        System.out.println(b);
+    }
+}
