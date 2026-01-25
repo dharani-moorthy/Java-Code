028d65fa91a663516cebe82af01ab52cd1dc211d
Dharani M <dharanimoorthy51@gmail.com>
2026-01-25 14:04:39 +0530

    Dec to Bin

---
+++ P7_Dec_To_Bin.java
@@ -0,0 +1,9 @@
+package PrepInsta_BasedOnNumbers;
+//Decimal to binary conversion using Java
+public class P7_Dec_To_Bin {
+    public static void main(String[] args) {
+        int a=12;
+        String b= Integer.toBinaryString(a);
+        System.out.println(b);
+    }
+}
