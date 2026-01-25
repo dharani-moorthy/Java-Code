fdb9cb871a6f39801a91462cc8704f99b2a3b04c
Dharani M <dharanimoorthy51@gmail.com>
2026-01-25 14:06:29 +0530

    Dec to Hex

---
+++ P9_Dec_To_Hex.java
@@ -0,0 +1,9 @@
+package PrepInsta_BasedOnNumbers;
+//Decimal to Hexadecimal Conversion in Java
+public class P9_Dec_To_Hex {
+    public static void main(String[] args) {
+        int a=1457;
+        String b=Integer.toHexString(a);
+        System.out.println(b);
+    }
+}
