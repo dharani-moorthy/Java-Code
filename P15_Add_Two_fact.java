71a3d7333477567bcd601959822cc59f30368956
Dharani M <dharanimoorthy51@gmail.com>
2026-01-28 15:42:07 +0530

    Add To Factorial

---
+++ P15_Add_Two_fact.java
@@ -0,0 +1,13 @@
+//package PrepInsta_BasedOnNumbers;
+////Add two fractions using Java
+///*
+//1   3    2
+//_ + _ =  _
+//3   9    3
+// */
+//public class P15_Add_Two_fact {
+//    public static void main(String[] args) {
+//        int num1;
+//
+//    }
+//}
