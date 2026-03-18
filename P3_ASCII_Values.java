d329a584e37efc5873bc679008d2e20655f1a876
Dharani M <dharanimoorthy51@gmail.com>
2026-03-18 21:08:59 +0530

    ASCII_Values

---
+++ P3_ASCII_Values.java
@@ -0,0 +1,9 @@
+package PrepInsta_Operation_on_String;
+//Java Program for Calculating ASCII value of a character
+public class P3_ASCII_Values {
+    public static void main(String[] args) {
+        char c='Z';
+        int a=c;
+        System.out.println(a);
+    }
+}
